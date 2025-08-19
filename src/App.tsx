e995bdd4dfe931650ae524ee550901801664624a
itaiglikman <itaig1998@gmail.com>
2025-08-19 17:46:46 +0300

    initial styling and dependencies setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { type event } from './utils/Types'
 
 function App() {
-  const [count, setCount] = useState<string | null | undefined>(null)
-
-  function handleChange(e:event) {
-    setCount(e.target.value);
-  }
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        
-          <input type="text" onChange={handleChange} />
-        
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <></>
+    )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+// ‼️ import charts styles after core package styles
+import '@mantine/charts/styles.css';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <MantineProvider>
+        <App />
+    </MantineProvider>
 )
