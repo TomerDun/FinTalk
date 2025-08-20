2d83a7d4b467eb9cb44aca105bcb4c16c81e4f8f
itaiglikman <itaig1998@gmail.com>
2025-08-20 14:10:00 +0300

    feat: implement layout in app and set css layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css'
+import { Navbar } from './components/layout/Navbar'
+import { Routing } from './components/layout/Routing/Routing'
 
 function App() {
     return (
-        <></>
+        <div className='App'>
+            <Navbar />
+            <Routing/>
+        </div >
     )
 }
 
