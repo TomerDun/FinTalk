115875f94f545568039ad0bf11ba8de7199a1ba9
itaiglikman <itaig1998@gmail.com>
2025-08-20 17:48:59 +0300

    feat: create auth components

---
+++ src/components/auth/Login/Login.tsx
@@ -0,0 +1,11 @@
+import "./Login.css";
+
+export function Login() {
+    return (
+        <div className="Login">
+
+			<p>Login Component</p>
+
+        </div>
+    );
+}
--- src/components/layout/Routing/Routing.tsx
+++ src/components/layout/Routing/Routing.tsx
@@ -1,11 +1,22 @@
-import "./Routing.css";
+import { Route, Routes } from "react-router";
+import { Dashboard } from "../../../pages/Dashboard";
+import { PageNotFound } from "../../../pages/PageNotFound/PageNotFound";
+import { Login } from "../../auth/Login/Login";
+import { Register } from "../../auth/Register/Register";
+import { Box } from "@mantine/core";
 
 export function Routing() {
     return (
-        <div className="Routing">
-
-			<p>Routing Component</p>
-
-        </div>
+        <Box flex={1}>
+            <Routes>
+                {/* feed - will change to the home page address*/}
+                <Route path="/feed" />
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                {/* 404 */}
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </Box>
     );
 }
