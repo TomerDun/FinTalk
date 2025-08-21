3d14435131b90cf7fb115a0023c80d19c7cd4a07
itaiglikman <itaig1998@gmail.com>
2025-08-21 20:16:48 +0300

    feat: register comp implementation with stepper

--- src/components/auth/Register/Register.tsx
+++ src/components/auth/Register/Register.tsx
@@ -1,25 +1,42 @@
+import { NumberInput, Select, TextInput } from "@mantine/core";
 import { useState } from "react";
-import "./Register.css";
+import { useNavigate } from "react-router";
+import { City, Gender, Profession, Status, validateRegisterFormByStep } from "../../../utils/formUtils";
+import classes from "./Register.module.css";
 import Stepper, { Step } from './Stepper';
+import { useRegisterForm } from "../../../utils/hooks/useRegisterForm";
 
 export function Register() {
-    const [nickname, setNickname] = useState('');
-    const [gender, setGender] = useState('');
-    const [age, setAge] = useState(25);
-    const [city, setCity] = useState('Haifa');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+
+    const [currentStep, setCurrentStep] = useState(1);
+
+    const form = useRegisterForm();
+    const navigate = useNavigate();
+
+    function handleStepChange(step: number) {
+        return validateRegisterFormByStep(form, step);
+    }
 
     return (
         <div className="Register">
             <Stepper
                 initialStep={1}
                 onStepChange={(step) => {
-                    console.log(step);
+                    setCurrentStep(step)
                 }}
-                onFinalStepCompleted={() => console.log("All steps completed!")}
+                onFinalStepCompleted={() => {
+                    setTimeout(() => {
+                        navigate('/')
+                    }, 1000)
+                }}
                 backButtonText="Previous"
                 nextButtonText="Next"
+                nextButtonProps={{
+                    onClick: (e) => {
+                        const isValid = handleStepChange(currentStep);
+                        if (isValid) e.preventDefault();
+                    }
+                }}
             >
                 <Step>
                     <h2>Welcome to FinTalk!</h2>
@@ -27,21 +44,93 @@ export function Register() {
                 </Step>
                 <Step>
                     <h2>Credentials</h2>
-                    <label htmlFor="email"></label>
-                    <input type="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email" />
-                    <input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password" />
+                    <TextInput
+                        withAsterisk
+                        label="Email"
+                        placeholder="your@email.com"
+                        key={form.key('email')}
+                        {...form.getInputProps('email')}
+                    />
+                    <TextInput
+                        withAsterisk
+                        type="password"
+                        label="Password"
+                        placeholder="your password"
+                        key={form.key('password')}
+                        {...form.getInputProps('password')}
+                    />
                 </Step>
                 <Step>
                     <h2>Let us get to know you better...</h2>
-                    <input type="text" value={nickname} onChange={(e) => setNickname(e.target.value)} placeholder="your anonymous nickname" />
-                    <input type="text" value={gender} onChange={(e) => setGender(e.target.value)} placeholder="your gender" />
-                    <input type="number" name="age" value={age} onChange={(e) => setAge(Number(e.target.value))} placeholder="your age" />
-                    <input type="string" name="city" value={city} onChange={(e) => setCity(e.target.value)} placeholder="your city" />
+                    <TextInput
+                        withAsterisk
+                        label="nickname"
+                        placeholder="your anonymous nickname"
+                        key={form.key('nickname')}
+                        {...form.getInputProps('nickname')}
+                    />
+                    <Select
+                        withAsterisk
+                        mt="md"
+                        comboboxProps={{ withinPortal: true }}
+                        data={Object.values(Gender)}
+                        placeholder="Pick one"
+                        label="Pick your Gender"
+                        classNames={classes}
+                        key={form.key('gender')}
+                        {...form.getInputProps('gender')}
+                    />
+                    <NumberInput
+                        withAsterisk
+                        placeholder="age"
+                        min={13}
+                        max={120}
+                        label="Pick your age"
+                        key={form.key('age')}
+                        {...form.getInputProps('age')}
+                    />
+                    <Select
+                        withAsterisk
+                        mt="md"
+                        comboboxProps={{ withinPortal: true }}
+                        data={Object.values(City)}
+                        placeholder="Pick your city"
+                        label="Your City"
+                        searchable
+                        nothingFoundMessage="Nothing found..."
+                        classNames={classes}
+                        key={form.key('city')}
+                        {...form.getInputProps('city')}
+                    />
                 </Step>
                 <Step>
                     <h2>Some more info to give you the best results</h2>
-                    <input type="string" name="profession" placeholder="your profession" />
-                    <input type="string" name="status" placeholder="your status" />
+                    <Select
+                        mt="md"
+                        comboboxProps={{ withinPortal: true }}
+                        data={Object.values(Profession)}
+                        placeholder="Pick your profession"
+                        label="Your profession"
+                        clearable
+                        searchable
+                        nothingFoundMessage="Nothing found..."
+                        classNames={classes}
+                        key={form.key('profession')}
+                        {...form.getInputProps('profession')}
+                    />
+                    <Select
+                        mt="md"
+                        comboboxProps={{ withinPortal: true }}
+                        data={Object.values(Status)}
+                        placeholder="Pick your status"
+                        label="Your status"
+                        clearable
+                        searchable
+                        nothingFoundMessage="Nothing found..."
+                        classNames={classes}
+                        key={form.key('status')}
+                        {...form.getInputProps('status')}
+                    />
                 </Step>
                 <Step>
                     <h2>Final Step</h2>
