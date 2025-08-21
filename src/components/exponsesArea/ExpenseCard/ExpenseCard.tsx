06e7a7bc14d2a45c0ac846606059852a1dd530f5
TomerDun <tomer@dunsky.name>
2025-08-21 16:23:48 +0300

    Addde styling for expense categories

--- src/components/exponsesArea/ExpenseCard/ExpenseCard.tsx
+++ src/components/exponsesArea/ExpenseCard/ExpenseCard.tsx
@@ -1,4 +1,5 @@
 import type { Expense } from "../../../stores/ProfileStore"
+import { IconCalendarEvent, IconTag } from "@tabler/icons-react"
 import './ExpenseCard.css'
 
 
@@ -6,19 +7,28 @@ type ExpenseCardProps = {
     expense: Expense
 }
 
-export default function ExpenseCard({expense}: ExpenseCardProps) {
+export default function ExpenseCard({ expense }: ExpenseCardProps) {
     return (
-        <div className="expense-container">
+        <div className="expense-container hover-shadow">
             <div className="header-row">
                 <span className="exp-name">Expense Name</span>
                 <span className="exp-amount">150$</span>
             </div>
 
-            <div className="buttom-row">
-                <span className="created">Sep 16, 25</span>
-                <div className="categories-container">
-                    <span>Category</span>
-                    <span>Sub Category</span>
+            <div className="bottom-row">
+                <span className="created">
+                    <IconCalendarEvent size={16} />
+                    Sep 16, 25
+                </span>
+                <div className="categories">
+                    <span className="category">
+                        <IconTag size={16} />
+                        {expense.category}
+                    </span>
+                    <span>
+                        <IconTag size={16}/>
+                        {expense.subCategory}
+                    </span>
                 </div>
             </div>
         </div>
