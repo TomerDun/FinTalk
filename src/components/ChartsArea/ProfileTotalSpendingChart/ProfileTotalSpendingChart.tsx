e8678a24366d5777e86487b7ad7b559feab9a08e
TomerDun <tomer@dunsky.name>
2025-08-23 19:56:50 +0300

    Added group function for dates and categories

--- src/components/ChartsArea/ProfileTotalSpendingChart/ProfileTotalSpendingChart.tsx
+++ src/components/ChartsArea/ProfileTotalSpendingChart/ProfileTotalSpendingChart.tsx
@@ -2,7 +2,7 @@ import { AreaChart } from "@mantine/charts";
 import '../ExpenseChart.css'
 import { observer } from "mobx-react-lite";
 import { profileStore } from "../../../stores/ProfileStore";
-import { groupExpensesByDate } from "../../../utils/expenseDataUtils";
+import { groupExpensesByDate, groupExpensesByDateAndCateogry } from "../../../utils/expenseDataUtils";
 
 function ProfileTotalSpendingChart() {
 
@@ -11,7 +11,7 @@ function ProfileTotalSpendingChart() {
     
 
     const chartSeries = [
-        { name: 'amount', color: 'indigo.6' }
+        { name: 'amount', color: '#000000' }
     ]
 
     console.log('Chart DATA: ', chartData);
@@ -21,6 +21,7 @@ function ProfileTotalSpendingChart() {
         <div className="expense-chart-container">
             <div className="header-row">
                 <h2>Total Spending Chart</h2>
+                <button onClick={() => console.log(groupExpensesByDateAndCateogry(profileStore.expenses))}>Data</button>
             </div>
 
             {chartData &&
--- src/utils/expenseDataUtils.ts
+++ src/utils/expenseDataUtils.ts
@@ -12,6 +12,13 @@ export type ExpensesByDateData = {
     amount: number
 }
 
+type FormattedExpense = {
+  date: string;
+  [category: string]: number| string; // dynamic category keys
+};
+
+
+// total amount by date
 export function groupExpensesByDate(expenses: Expense[]): ExpensesByDateData[] {
     const expenseByDates:ExpensesByDates = expenses.reduce<ExpensesByDates>((acc, expense) => {
         if (acc[expense.date.toDateString()]) {
@@ -23,4 +30,24 @@ export function groupExpensesByDate(expenses: Expense[]): ExpensesByDateData[] {
     }, {});
 
     return formatExpensesByDate(expenseByDates)
+}
+
+// Total amount by date and category
+
+export function groupExpensesByDateAndCateogry(expenses: Expense[]): FormattedExpense[] {
+  const groupedMap = new Map<string, Record<string, number>>();
+
+  for (const exp of expenses) {
+    const dateKey = exp.date.toDateString(); // YYYY-MM-DD
+    const categoryTotals = groupedMap.get(dateKey) ?? {};
+
+    categoryTotals[exp.category] = (categoryTotals[exp.category] || 0) + exp.amount;
+    groupedMap.set(dateKey, categoryTotals);
+  }
+
+  // Convert Map to array of objects with 'date' included
+  return Array.from(groupedMap.entries()).map(([date, categories]) => ({
+    date,
+    ...categories,
+  }));
 }
