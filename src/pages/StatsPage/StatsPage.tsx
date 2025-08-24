de6e8db9ad5db195d5f9c834ce8e4187d1509ad6
TomerDun <tomer@dunsky.name>
2025-08-24 14:38:09 +0300

    Finished stats page

--- src/pages/StatsPage/StatsPage.tsx
+++ src/pages/StatsPage/StatsPage.tsx
@@ -3,18 +3,31 @@ import type { Expense } from "../../stores/ProfileStore"
 import { fetchAllExpenses } from "../../utils/apiUtils/expenseApiUtils"
 import SpendingInCategoryChart from "../../components/ChartsArea/GeneralCharts/SpendingInCategoryChart/SpendingInCategoryChart"
 import CategorySplitChart from "../../components/ChartsArea/GeneralCharts/CategorySplitChart/CategorySplitChart"
+import './StatsPage.css'
+import { IconCurrencyDollar, IconTrendingUp, IconUser } from "@tabler/icons-react"
+import CountUp from "../../components/MiscArea/CountUp/CountUp"
+import { fetchProfileCount } from "../../utils/apiUtils/profileApiUtils"
 
 export default function StatsPage() {
 
     const [expenses, setExpenses] = useState<Expense[]>([])
-    
+    const [profileCount, setProfileCount] = useState<number | null>(null);
+
     useEffect(() => {
         getExpenses();
+        getProfileCount();
     }, [])
+
+    const expensesTotal: number | null = expenses.length ? expenses.reduce((sum, curr) => sum += curr.amount, 0) : null
 
     async function getExpenses() {
         const newExpenses = await fetchAllExpenses();
-        setExpenses(newExpenses)   ;
+        setExpenses(newExpenses);
+    }
+
+    async function getProfileCount() {
+        const newCount = await fetchProfileCount();
+        setProfileCount(newCount)
     }
 
     if (!expenses.length) {
@@ -30,13 +43,38 @@ export default function StatsPage() {
             </div>
 
             <div className="general-stats-area">
-                General Area
+                {expenses &&
+                    <>
+
+                        <div className="stat-container">
+                            <IconTrendingUp size={52} color="#3b82f6" />
+                            {expensesTotal && <CountUp to={expensesTotal} className="amount" /> }
+                            <span>Total Spent</span>
+                        </div>
+
+
+                        <div className="stat-container">
+                            <IconCurrencyDollar size={52} color="rgb(16,185,129)" />
+                            <CountUp to={expenses.length} className="amount" />
+                            <span>Transactions Recorded</span>
+                        </div>
+
+                        <div className="stat-container">
+                            <IconUser size={52} color="#a855f7" />
+
+                            {profileCount && <CountUp to={profileCount} className="amount" />}
+
+                            <span>Users in the community</span>
+
+                        </div>
+                    </>
+                }
             </div>
 
             <div className="charts-area">
                 <SpendingInCategoryChart />
 
-                <CategorySplitChart expenses={expenses}/>
+                <CategorySplitChart expenses={expenses} />
             </div>
         </div>
     )
--- src/utils/apiUtils/profileApiUtils.ts
+++ src/utils/apiUtils/profileApiUtils.ts
@@ -1,14 +1,27 @@
 import { supabase } from "./supabaseUtils";
 
 // export async function fetchProfile(userId = 'c7f9877b-7b48-4b13-a2e7-9c83f24bfd63') {
-export async function fetchProfile(userId="c7f9877b-7b48-4b13-a2e7-9c83f24bfd63") {
-    const {data, error} = await supabase.from('profiles').select('*').eq('userId', userId);
+export async function fetchProfile(userId = "c7f9877b-7b48-4b13-a2e7-9c83f24bfd63") {
+    const { data, error } = await supabase.from('profiles').select('*').eq('userId', userId);
 
-    if(error) {
+    if (error) {
         console.error('Supabase error when fetching profile: ', error)
         return error;
     }
-    console.log('--fetched profile with id ', userId);   
-    
+    console.log('--fetched profile with id ', userId);
+
     return data[0];
+}
+
+export async function fetchProfileCount() {
+    const { count, error } = await supabase.from('profiles').select('*', {count: 'exact'})
+
+    if (error) {
+        console.error('Supabase error when fetching profile: ', error)
+        throw new Error('Error fetching profile count')
+    }    
+
+    console.log(count);
+    
+    return count;
 }
